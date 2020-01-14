9845acaf9f68a82b950c8e012f0f85e3b7a9ab81
Nami Izarra <nami.izarra@gmail.com>
2020-01-14 15:41:03 +0900

    added city list and city

--- src/containers/city.jsx
+++ src/containers/city.jsx
@@ -1,1 +1,13 @@
-city.jsx
+import React from 'react';
+
+class City extends React.Component {
+  render() {
+    return (
+      <div className="list-group-item">
+        { this.props.city.name }
+      </div>
+    );
+  }
+}
+
+export default City;
--- src/containers/city_list.jsx
+++ src/containers/city_list.jsx
@@ -1,1 +1,14 @@
-city_list.jsx
+import React from 'react';
+import City from './city'
+
+class CityList extends React.Component {
+  render() {
+    return (
+      <div className="cities">
+        { this.props.cities.map(city => <City key={ city.name } city={ city } />)}
+      </div>
+    );
+  }
+}
+
+export default CityList;
